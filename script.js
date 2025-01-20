e94d5f3e6db310084e51f467f73752872d646465
Muskan <muskan.goyal@liferay.com>
2025-01-20 15:20:22 +0530

    updated css

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+/* Welcome, Marla! */
+
+width: 282px;
+height: 36px;
+
+font-family: 'Frutiger LT Pro';
+font-style: normal;
+font-weight: 700;
+font-size: 30px;
+line-height: 36px;
+/* identical to box height, or 120% */
+display: flex;
+align-items: center;
+text-transform: uppercase;
+
+/* Neutral/10 */
+color: #101010;
 
 
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
